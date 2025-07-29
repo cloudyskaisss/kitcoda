fc9ae9f30646f8d9cdc6ffc7b46908646399f74f
cloudyskaisss <146793021+cloudyskaisss@users.noreply.github.com>
2025-07-28 22:22:54 -0700

    Update kitcoda.py
    Added a sit (set) command.
    Added a sleep (exit) command.
    Fixed bugs.

--- kitcoda.py
+++ kitcoda.py
@@ -1,36 +1,55 @@
 import os
 import sys
 
-DIR = ""
-FILECONTENTS = ""
 
 
 def main():
+    i = 0
+    variables = {}
     if len(sys.argv) > 1:
         file = sys.argv[1]
         if os.path.exists(file) and file[-4:] == ".kit":
-            DIR = sys.argv[1]
+            dir = sys.argv[1]
         else:
             print("Invalid directory.")
             exit()
 
-    with open(DIR, "r") as d:
-        FILECONTENTS = d.read()
-    if FILECONTENTS != "":
-        FILECONTENTS = FILECONTENTS
+    with open(dir, "r") as d:
+        filecontents = d.read()
+    if filecontents != "":
+        filecontents = filecontents
     else:
         print("File is empty.")
         exit()
     
 
-    lines = FILECONTENTS.splitlines()
+    lines = filecontents.splitlines()
     for line in lines:
         words = line.split()
-        for word in words:
-            if word == "meow":
-                printstr = line.split()[1:]
-                if printstr:
-                    for str in printstr:
-                        print(str + " ")
+        cmd = words[0]
+        if cmd == "meow":
+            printstr = line.split()[1:]
+            if printstr:
+                for str in printstr:
+                    for var in variables:
+                        if str == var:
+                            for v in variables[var]:
+                                print(v)
+                            return
+                    print(str + " ")
+            else:
+                print("Invalid syntax.")
+        elif cmd == "sit":
+            if words[1] and words[2] == "is" and words[3]:
+                varname = words[1]
+                varvalue = words[3:]
+                tempvarvalue = ""
+                for f in varvalue:
+                    tempvarvalue = tempvarvalue + f
+                varvalue = tempvarvalue
+                variables[varname] = [varvalue]
+        elif cmd == "sleep":
+            exit()
+        i += 1
 
 main()
